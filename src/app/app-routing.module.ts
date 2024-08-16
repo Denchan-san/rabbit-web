5131826c5b5b31f75de8dd692f3e464e28f4432e
Jokir <doner24072004@gmail.com>
2024-08-16 16:45:42 +0300

    Added AppRouting Module

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+
+const appRoutes: Routes = [
+    {path: '', redirectTo: '/threads', pathMatch: 'full'}
+]
+
+@NgModule({
+    imports: [RouterModule.forRoot(appRoutes)]
+})
+export class AppRoutingModule {}
