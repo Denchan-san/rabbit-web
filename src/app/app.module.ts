c9cc8ab5718ba5539c47804564cefd7563f834b3
Jokir <doner24072004@gmail.com>
2024-08-18 20:41:17 +0300

    Post Components were Added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,13 @@ import { ThreadsModule } from './threads/threads.module';
 import { AppRoutingModule } from './app-routing.module';
 import { RouterModule } from '@angular/router';
 import { ThreadsRoutingModule } from './threads/threads-routing.module';
+import { PostsComponent } from './posts/posts.component';
+import { PostListComponent } from './posts/post-list/post-list.component';
+import { PostItemComponent } from './posts/post-list/post-item/post-item.component';
+import { PostEditComponent } from './posts/post-edit/post-edit.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent],
+  declarations: [AppComponent, HeaderComponent, PostsComponent, PostListComponent, PostItemComponent, PostEditComponent],
   imports: [
     ThreadsModule,
     BrowserModule,
