6c63acd1fd8f57a4e6398d009f8b368f75ebf2ba
Jokir <doner24072004@gmail.com>
2024-08-19 20:37:32 +0300

    Added Post-List and Item components with logic init

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
+import { Subscription } from 'rxjs';
+
+import { Post } from '../post.model';
+import { PostService } from '../post.service';
 
 @Component({
   selector: 'app-post-list',
   templateUrl: './post-list.component.html',
-  styleUrl: './post-list.component.css'
+  styleUrl: './post-list.component.css',
 })
-export class PostListComponent {
-  
+export class PostListComponent implements OnInit, OnDestroy {
+  posts: Post[] = [];
+  subscription: Subscription;
+
+  constructor(
+    private postService: PostService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit() {
+    this.subscription = this.postService.postsChanged.subscribe(
+      (posts: Post[]) => {
+        this.posts = posts;
+      }
+    );
+    this.posts = this.postService.getPosts();
+  }
+
+  onNewPost() {
+    this.router.navigate(['new', { relativeTo: this.route }]);
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 }
