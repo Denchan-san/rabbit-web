ee3d968be719e81bf0073a004cc3604e8b59503a
Jokir <doner24072004@gmail.com>
2024-08-25 19:30:12 +0300

    Added functional of ThreadEdit

--- src/app/threads/thread-edit/thread-edit.component.ts
+++ src/app/threads/thread-edit/thread-edit.component.ts
@@ -1,30 +1,71 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ThreadService } from '../thread.service';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 @Component({
   selector: 'app-thread-edit',
   templateUrl: './thread-edit.component.html',
-  styleUrl: './thread-edit.component.css'
+  styleUrl: './thread-edit.component.css',
 })
-export class ThreadEditComponent implements OnInit{
+export class ThreadEditComponent implements OnInit {
   id: number;
   editMode = false;
   threadForm: FormGroup;
 
-  constructor(private threadService: ThreadService) {
-    
-  }
+  constructor(
+    private threadService: ThreadService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
 
   ngOnInit() {
-    
+    this.route.params.subscribe((params: Params) => {
+      this.id = +params['id'];
+      this.editMode = params['id'] != null;
+      this.initForm();
+    });
   }
 
   ngSubmit() {
-
+    if (this.editMode) {
+      this.threadService.updateThread(this.id, this.threadForm.value);
+    } else {
+      this.threadService.addThread(this.threadForm.value);
+    }
+    this.onCancel();
   }
-  
-  onCancel() {
 
+  onCancel() {
+    this.router.navigate(['../'], { relativeTo: this.route });
+  }
+
+  private initForm() {
+    let threadTitle = '';
+    let threadDescription = '';
+    let threadImageUrl = '';
+    // let userId = authService.currentUserId;
+    let threadUserId = 1;
+    let threadUpdatedDate = new Date();
+    let createdDate: Date;
+
+    if (this.editMode) {
+      const thread = this.threadService.getThread(this.id);
+      threadTitle = thread.title;
+      threadDescription = thread.description;
+      threadImageUrl = thread.imageUrl;
+      createdDate = thread.createdDate;
+    } else {
+      createdDate = new Date();
+    }
+
+    this.threadForm = new FormGroup({
+      title: new FormControl(threadTitle, Validators.required),
+      description: new FormControl(threadDescription, Validators.required),
+      imageUrl: new FormControl(threadImageUrl),
+      userId: new FormControl(threadUserId),
+      createdDate: new FormControl(createdDate),
+      updatedDate: new FormControl(threadUpdatedDate),
+    });
   }
 }
